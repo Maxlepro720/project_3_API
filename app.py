e8da73804fbf630123651d0fdec581b243908d47
Maxlepro720 <maxencecera@gmail.com>
2026-01-06 16:17:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,6 @@ def add_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = 'https://clickerbutmultiplayer.xo.je'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
     return response
 
 @app.route('/<path:path>', methods=['OPTIONS'])
@@ -38,7 +37,6 @@ def options_handler(path):
     response.headers['Access-Control-Allow-Origin'] = 'https://clickerbutmultiplayer.xo.je'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
     return response, 200
 
 # NOTE : Assurez-vous que ces variables d'environnement sont bien définies
