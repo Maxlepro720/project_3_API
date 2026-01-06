f76b3848dffd21ee7c1b9cdaab12246d7fee07b9
Maxlepro720 <maxencecera@gmail.com>
2026-01-06 15:55:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ def options_handler(path):
     response.headers['Access-Control-Allow-Origin'] = 'https://clickerbutmultiplayer.xo.je'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
     return response, 200
 
 # NOTE : Assurez-vous que ces variables d'environnement sont bien définies
