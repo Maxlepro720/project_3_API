46360149c3d5bf163aab0449c5519019f76741f6
Maxlepro720 <maxencecera@gmail.com>
2026-01-06 16:09:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def add_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = 'https://clickerbutmultiplayer.xo.je'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
     return response
 
 @app.route('/<path:path>', methods=['OPTIONS'])
